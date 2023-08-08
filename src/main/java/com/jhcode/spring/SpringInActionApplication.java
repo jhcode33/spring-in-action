dee4906b87f0b7d12b7c3c244d07ee21fba19ae4
jhcode33 <jhcode33@gmail.com>
2023-08-08 15:59:04 +0900

    init Spring In Action

---
+++ src/main/java/com/jhcode/spring/SpringInActionApplication.java
@@ -0,0 +1,16 @@
+package com.jhcode.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Indexed;
+import org.springframework.stereotype.Service;
+
+@SpringBootApplication
+public class SpringInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInActionApplication.class, args);
+    }
+
+}
